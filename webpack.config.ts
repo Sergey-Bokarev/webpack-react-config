50cc07087269cd871734ec1370c97bdf5104f590
Sergey Bokarev <bokarev_87@mail.ru>
2024-06-18 11:18:37 +0300

    init repo

---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+type Mode = 'production' | 'development';
+
+interface EnvVariables {
+    mode: Mode,
+    port: number
+}
+
+export default (env: EnvVariables) => {
+
+    const isDev = env.mode === 'development';
+
+    const config: webpack.Configuration = {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js',
+            clean: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js']
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })
+        ],
+        devtool: isDev ? "inline-source-map" : false,
+        devServer: isDev ? {
+            port: env.port ?? 5000,
+            open: true
+        } : undefined
+    }
+
+    return config;
+}
